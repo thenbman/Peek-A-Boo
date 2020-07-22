f37f0c210ad8c231007215dcac796b59bcb42d55
thenbman <thenbman@hotmail.co.uk>
2020-07-22 01:26:33 +0100

    Add files via upload

---
+++ peek.js
@@ -0,0 +1,21 @@
+    
+function peek_onmouseover() {
+     doctortext.style.visibility = "visible";
+      doctor.style.visibility = "visible";
+      elementtext.style.visibility = "hidden";
+     element.style.visibility = "hidden";
+   }
+
+    function aboo_onmouseover() {
+      doctortext.style.visibility = "hidden";
+      doctor.style.visibility = "hidden";
+     elementtext.style.visibility = "visible";
+      element.style.visibility = "visible";
+    }
+
+    function everyone_onmouseout() {
+      doctortext.style.visibility = "visible";
+      doctor.style.visibility = "visible";
+      elementtext.style.visibility = "visible";
+      element.style.visibility = "visible";
+    }
